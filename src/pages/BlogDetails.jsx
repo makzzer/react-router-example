390c0d756a623f90801fb391d595ef67c7b49083
Maximiliano Sanchez <makzofx@gmail.com>
2023-05-18 03:46:13 -0300

    agregado useParams en BlogDetails

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,7 +1,39 @@
-const BlogDetails = () => { 
-    return (
-        <h1>Blog details variable</h1>
-    )
- }
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
- export default BlogDetails;
+const BlogDetails = () => {
+
+    //capturo el parametro que recibo cuando le doy click a la lista de articulos que llegan desde la API
+    const idParametro = useParams()
+    console.log(idParametro)
+
+    const { data, loading, error } = useFetch(`https://jsonplaceholder.typicode.com/posts/${idParametro.id}`);
+
+    if (error) {
+        <h1>Error....</h1>
+
+    } if (loading) {
+        <h1>Loading...</h1>
+
+    } else {
+
+        return (
+            <>
+
+                {
+                    data ?
+
+                        <>
+                            <h1>Blog details variable</h1>
+                            <h1>{data.id} </h1>
+                            <h2>{data.title}</h2>
+                            <Link to="/blog" className="btn btn-outline-danger">Volver</Link>
+                        </>
+
+                        : <h1>LOADING...</h1>
+                }
+            </>
+        )
+    }
+}
+export default BlogDetails;
