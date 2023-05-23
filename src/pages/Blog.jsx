198d67e7af16d6f5a7ee81f09e25a3352ffcab07
Makz Sánchez <makzofx@gmail.com>
2023-05-23 01:53:52 -0300

    seteo de getParams de filtr de URL

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,61 +2,61 @@ import { useEffect } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { Link, useSearchParams } from "react-router-dom";
 
-
 const Blog = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
 
-
-    const [searchParams, setSearchParams] = useSearchParams()
-
-    //pseudo queris para hacer una busqueda con cierto parametro en la barra del navegador
+  /*pseudo queris para hacer una busqueda con cierto parametro en la barra del navegador
     useEffect(() => {
         console.log(searchParams.get('name'));
     }
         , [searchParams])
+*/
 
-
-       const handleChange = () =>{
-        console.log("change")
-       }
-
-
-    const { data, loading, error } = useFetch("https://jsonplaceholder.typicode.com/posts");
-
-    if (error) {
-
-        return <p>error...</p>
+  const handleChange = (e) => {
+    let loQueEscribioElUsuarioEnElFiltro = e.target.value;
+    {
+      /*En lo siguiente le seteo que para el "filter" que definí en el value del input dentro de searchParams.get("filter"), y ese filter yo digo que va a ser igual a lo que escribe el usuario en el input*/
     }
-    if (loading) {
-        return <p>loading...</p>
+    setSearchParams({ filter: loQueEscribioElUsuarioEnElFiltro });
+  };
 
-    } else {
-        return (
-            <>
-                <h1>Blog de cositas</h1>
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
 
+  if (error) {
+    return <p>error...</p>;
+  }
+  if (loading) {
+    return <p>loading...</p>;
+  } else {
+    return (
+      <>
+        <h1>Blog de cositas</h1>
 
-                <input type="text" name="filtroBlog" onChange={handleChange} className="form-control my-3">
-                </input>
+        <input
+          type="text"
+          name="filtrotonto"
+          value={searchParams.get("filter") || ""}
+          onChange={handleChange}
+          className="form-control my-3"
+        ></input>
 
-
-
-                <ul className="list-group">
-                    {/*con agregarle al Link el prop to={`/blog/${dato.id}`}, hago que cuando clickee en el enlace me cree la pagina directamente del blog que elijo ver, eso lo puedo hacer en una tienda para que me lleve al articulo puntual y no tener 100 paginas para diferentes objetos*/}
-                    {
-                        data.map(dato =>
-                        (
-                            <Link to={`/blog/${dato.id}`} key={dato.id}
-                                className="list-group-item">
-                                {dato.id} {dato.title}
-                            </Link>)
-                        )
-                    }
-                </ul>
-            </>
-        )
-    }
-}
-
-
+        <ul className="list-group">
+          {/*con agregarle al Link el prop to={`/blog/${dato.id}`}, hago que cuando clickee en el enlace me cree la pagina directamente del blog que elijo ver, eso lo puedo hacer en una tienda para que me lleve al articulo puntual y no tener 100 paginas para diferentes objetos*/}
+          {data.map((dato) => (
+            <Link
+              to={`/blog/${dato.id}`}
+              key={dato.id}
+              className="list-group-item"
+            >
+              {dato.id} {dato.title}
+            </Link>
+          ))}
+        </ul>
+      </>
+    );
+  }
+};
 
 export default Blog;
